8272664aad8a5d1eda236a503ec034d28a59c67e
yangdong weng <461662596@qq.com>
2024-03-23 19:03:27 +0800

    24:00处理

--- src/composables/useTimeNodes.ts
+++ src/composables/useTimeNodes.ts
@@ -2,30 +2,30 @@
 export function useTimeNodes(
     start: string = '08:00',
     end: string = '24:00',
-    step: string = '02:30',
-    fmt?: string
+    step: string = '02:00',
+    fmt: string = 'HH:mm'
 ) {
-    const getHourMinuteSeconds = (time: string) => {
+    const getHourMinuteSeconds = (time: string): number[] => {
         const [hour, minute, second] = time.split(':').map(Number)
         return [hour || 0, minute || 0, second || 0]
     }
 
-    const parseTime = (time: string) => {
+    const parseTime = (time: string): Date => {
         const [hour, minute, second] = getHourMinuteSeconds(time)
         const now = new Date()
         return new Date(now.getFullYear(), now.getMonth() + 1, now.getDate(), hour, minute, second)
     }
 
-    const getNodeTimes = (date: Date) => {
-        return formatDateTime(date, fmt || 'HH:mm')
+    const getNodeTimes = (date: Date): string => {
+        return formatDateTime(date, fmt)
     }
 
-    const addTimes = (date: Date) => {
+    const addTimes = (date: Date): Date => {
         const [hour, minute, second] = getHourMinuteSeconds(step)
         return new Date(date.getTime() + hour * 3600000 + minute * 60000 + second * 1000)
     }
 
-    const formatDateTime = (date: Date, fmtStr: string) => {
+    const formatDateTime = (date: Date, fmtStr: string): string => {
         const year = date.getFullYear()
         const month = date.getMonth() + 1
         const day = date.getDate()
@@ -50,12 +50,11 @@ export function useTimeNodes(
         return fmtStr.replace(/(yyyy|yy|MM|M|dd|d|HH|H|mm|m|ss|s)/g, (match) => maps[match])
     }
 
-    const timeNodes: any[] = []
+    const timeNodes: string[] = []
 
     const [hour, minute, second] = getHourMinuteSeconds(step)
-    if (hour + minute + second === 0) {
-        console.error('时间步长不能为0')
-        return { timeNodes, formatDateTime }
+    if (hour === 0 && minute === 0 && second === 0) {
+        throw new Error('时间步长不能为0')
     }
 
     let currentTime = parseTime(start)
@@ -66,7 +65,11 @@ export function useTimeNodes(
         currentTime = addTimes(currentTime)
     }
 
-    if (timeNodes.length > 1 && timeNodes[timeNodes.length - 1] == '00:00') {
+    //处理24:00
+    if (
+        timeNodes.length > 1 &&
+        String(getHourMinuteSeconds(timeNodes[timeNodes.length - 1])[0]).padStart(2, '0') === '00'
+    ) {
         timeNodes[timeNodes.length - 1] = '24:00'
     }
     return { timeNodes, formatDateTime }
